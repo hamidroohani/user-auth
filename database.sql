942c0cec5a5c918d85b2591a3408fb6508093953
HamidRoohani <hamid.dev@yahoo.com>
2021-11-05 14:03:32 +0330

    Update database.sql

--- database.sql
+++ database.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 14, 2021 at 07:51 PM
--- Server version: 8.0.25-0ubuntu0.20.04.1
+-- Generation Time: Nov 05, 2021 at 10:30 AM
+-- Server version: 8.0.27-0ubuntu0.20.04.1
 -- PHP Version: 7.3.27-1+ubuntu20.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -19,42 +19,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `shorter`
+-- Database: `auth-user`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `links`
---
-
-CREATE TABLE `links` (
-  `id` int NOT NULL,
-  `url` text COLLATE utf8_unicode_ci NOT NULL,
-  `slug` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tokens`
---
-
-CREATE TABLE `tokens` (
-  `user_id` int NOT NULL,
-  `token` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` datetime NOT NULL,
-  `expire_date` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
-
---
--- Dumping data for table `tokens`
---
-
-INSERT INTO `tokens` (`user_id`, `token`, `created_at`, `expire_date`) VALUES
-(1, 'y2MZKx0PThrhP6D332k1ZsUHjLJ1E9', '2021-07-14 18:49:26', '2021-07-15 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -63,7 +29,7 @@ INSERT INTO `tokens` (`user_id`, `token`, `created_at`, `expire_date`) VALUES
 --
 
 CREATE TABLE `users` (
-  `id` int UNSIGNED NOT NULL,
+  `id` int NOT NULL,
   `name` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(191) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -77,42 +43,27 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Admin', 'admin@example.com', 'e10adc3949ba59abbe56e057f20f883e', NULL, '2021-07-14 17:59:45', '2021-07-14 17:59:45');
+(1, 'Admin', 'admin@example.com', 'e10adc3949ba59abbe56e057f20f883e', NULL, '2021-07-14 13:29:45', '2021-07-14 13:29:45');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `links`
---
-ALTER TABLE `links`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `slug` (`slug`),
-  ADD KEY `slug_2` (`slug`);
-
---
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `links`
---
-ALTER TABLE `links`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

4c6fa406b7ac60e4f52f48dc852f0476d1f4652e
Hamid <hamid.roohany@gmail.com>
2022-10-24 17:26:46 +0330

    Add name spaces and routes

--- public/index.php
+++ public/index.php
@@ -2,7 +2,27 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use App\Http\Kernel;
+use App\Controllers\HomeController;
+use App\Controllers\LoginController;
+use App\Http\Router;
 
+$router = new Router();
 
-$app = new Kernel();
+//"ProductController::class"
+$router->add("GET", "", HomeController::class, "index");
+$router->add("GET", "login", LoginController::class, "index");
+$router->add("GET", "register", LoginController::class, "index");
+
+try {
+    echo $router->dispatch();
+} catch (Exception $exception) {
+    $errorCode = $exception->getCode();
+        $errorCode = 500;
+    $data = [
+        "errorMessage" => $exception->getMessage(),
+        "errorCode" => $errorCode
+    ];
+    header('Content-Type: application/json');
+    http_response_code($errorCode);
+    echo json_encode(["data" => $data], JSON_PRETTY_PRINT);
+}

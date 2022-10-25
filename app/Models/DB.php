a59b5eb66a5a46c6b182aa929c90e4a62570f205
Hamid <hamid.roohany@gmail.com>
2022-10-25 08:48:23 +0330

    Refactor models

--- app/Models/DB.php
+++ app/Models/DB.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use App\Http\Config;
 
-class DB
+abstract class DB
 {
     public $connstr = null;
     public $table = "users";
@@ -31,5 +31,47 @@ class DB
         if (!count($result)){
             Response::db_connection_not_exists();
         }
+    }
+
+
+    public function where($field, $opr, $val)
+    {
+        $this->query .= " and {$field} {$opr} '{$val}'";
+
+        return $this;
+    }
+
+    public function orWhere($field, $opr, $val)
+    {
+        $this->query .= " or {$field} {$opr} '{$val}'";
+
+        return $this;
+    }
+
+    public function get()
+    {
+        $result = $this->fetch();
+
+        return $result;
+    }
+
+    public function first()
+    {
+        $result = $this->fetch();
+
+        return $result[0];
+    }
+
+    /**
+     * @return array
+     */
+    public function fetch(): array
+    {
+        $this->query = mysqli_query($this->connstr, $this->query);
+        $result = [];
+        while ($res = mysqli_fetch_assoc($this->query)) {
+            array_push($result, $res);
+        }
+        return $result;
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -15,45 +15,4 @@ class User extends DB
         $this->query = "select * from " . $this->table . " where 1 ";
         return $this;
     }
-
-    public function where($field, $opr, $val)
-    {
-        $this->query .= " and {$field} {$opr} '{$val}'";
-
-        return $this;
-    }
-
-    public function orWhere($field, $opr, $val)
-    {
-        $this->query .= " or {$field} {$opr} '{$val}'";
-
-        return $this;
-    }
-
-    public function get()
-    {
-        $result = $this->fetch();
-
-        return $result;
-    }
-
-    public function first()
-    {
-        $result = $this->fetch();
-
-        return $result[0];
-    }
-
-    /**
-     * @return array
-     */
-    public function fetch(): array
-    {
-        $this->query = mysqli_query($this->connstr, $this->query);
-        $result = [];
-        while ($res = mysqli_fetch_assoc($this->query)) {
-            array_push($result, $res);
-        }
-        return $result;
-    }
 }
